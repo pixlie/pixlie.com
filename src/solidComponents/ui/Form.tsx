1b14be37ca555b6561b2a3520fb4c690e60bb870
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2024-10-30 22:06:36 -0400

    Demo UI WIP

--- src/solidComponents/ui/Form.tsx
+++ src/solidComponents/ui/Form.tsx
@@ -33,7 +33,7 @@ const Input: Component<IInputProps> = (props) => {
         id={props.name}
         autocomplete={props.autocomplete}
         placeholder={props.placeholder}
-        class="py-3 px-4 block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900"
+        class="py-3 px-4 outline-none block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900"
         value={props.value}
         onChange={props.onChange}
       />
@@ -67,4 +67,78 @@ const Textarea: Component<ITextareaProps> = (props) => {
   );
 };
 
-export { Label, Input, Textarea };
+interface IDropdownProps {
+  label?: string;
+  name?: string;
+  options: string[];
+  value?: string | number | string[] | undefined;
+  placeholder?: string;
+  onChange?: (e: Event) => void | undefined;
+}
+
+const Dropdown: Component<IDropdownProps> = (props) => {
+  return (
+    <div class="mb-6">
+      {!!props.label && <Label for={props.name} label={props.label} />}
+      <div class="px-4 block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900">
+        <select
+          id={props.name}
+          name={props.name}
+          class="py-3 block w-full bg-white dark:bg-slate-900 outline-none"
+          value={props.value || props.placeholder}
+          onChange={props.onChange}
+        >
+          <option value={props.placeholder}>{''}</option>
+          {props.options.map((option) => (
+            <option value={option}>{option}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+};
+
+interface ISliderProps {
+  label?: string;
+  name?: string;
+  value?: number;
+  min?: number;
+  max?: number;
+  step?: number;
+  prefix?: string;
+  suffix?: string;
+  onChange?: (e: Event) => void | undefined;
+}
+
+const Slider: Component<ISliderProps> = (props) => {
+  return (
+    <div class="mb-6">
+      {!!props.label && <Label for={props.name} label={props.label} />}
+      <input
+        type="range"
+        id="slider"
+        name={props.name}
+        min={props.min}
+        max={props.max}
+        onChange={props.onChange}
+        value={props.value}
+        class="py-3 px-4 block w-full text-md rounded-lg border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900"
+      />
+      <div class="flex justify-between">
+        <span>
+          {props.prefix}
+          {props.min}
+          {props.suffix}
+        </span>
+        <span>
+          {props.prefix}
+          {props.max}
+          {'+'}
+          {props.suffix}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export { Label, Input, Textarea, Dropdown, Slider };
