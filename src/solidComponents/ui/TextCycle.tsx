0f4a5dea6b75a9874d6ebf685ec5b371f61d7317
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2024-11-03 13:14:46 -0500

    text with scroll animation

---
+++ src/solidComponents/ui/TextCycle.tsx
@@ -0,0 +1,32 @@
+import { createSignal, onCleanup, type Component, onMount } from 'solid-js';
+
+const lines = ['Founders', 'Investors', 'Competitors', 'Partnerships'];
+
+const TextCycle: Component = () => {
+  const [currentLineIndex, setCurrentLineIndex] = createSignal(0);
+  const [transitioning, setTransitioning] = createSignal(false);
+
+  onMount(() => {
+    setCurrentLineIndex(0);
+    const interval = setInterval(() => {
+      setTransitioning(true);
+      setTimeout(() => {
+        setCurrentLineIndex((prev) => (prev + 1) % lines.length);
+        setTransitioning(false);
+      }, 200);
+    }, 2000);
+    onCleanup(() => clearInterval(interval));
+  });
+
+  return (
+    <div class="text-scroller">
+      <h1
+        class={`text-7xl md:text-8xl leading-tighter tracking-tighter font-heading dark:text-gray-200 line ${transitioning() ? 'slide-out' : 'slide-in'}`}
+      >
+        {lines[currentLineIndex()]}
+      </h1>
+    </div>
+  );
+};
+
+export default TextCycle;
