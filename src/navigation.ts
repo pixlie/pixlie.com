6c4345ce6ac16617b5eb6dcfa6c19fc64459ab01
Sumit Datta <sumitdatta@gmail.com>
2024-11-19 18:34:18 +0530

    Updated homepage, insights; changed navigation

--- src/navigation.ts
+++ src/navigation.ts
@@ -110,14 +110,18 @@ export const headerData = {
     //     },
     //   ],
     // },
+    // {
+    //   text: 'Pixlie AI',
+    //   href: '/ai',
+    // },
     {
-      text: 'Pixlie AI',
-      href: '/ai',
+      text: 'Insights',
+      href: '/insights',
     },
-    {
-      text: 'Dwata',
-      href: '/dwata',
-    },
+    // {
+    //   text: 'Dwata',
+    //   href: '/dwata',
+    // },
     {
       text: 'About',
       href: '/about',
@@ -125,10 +129,6 @@ export const headerData = {
     {
       text: 'Contact',
       href: '/contact',
-    },
-    {
-      text: 'Demo',
-      href: '/demo',
     },
   ],
   // actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
@@ -167,9 +167,9 @@ export const footerData = {
     {
       title: 'Company',
       links: [
+        { text: 'Insights', href: '/insights' },
         { text: 'About', href: '/about' },
         { text: 'Contact', href: '/contact' },
-        { text: 'Subscribe', href: '/subscribe' },
         // { text: 'Blog', href: '#' },
         // { text: 'Careers', href: '#' },
         // { text: 'Press', href: '#' },
@@ -193,12 +193,12 @@ export const footerData = {
     // { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/pixlie' },
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/company/pixlie' },
     // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-   // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pixlie' },
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
-//   footNote: `
-//     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
-//     Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
-//   `,
- };
+  //   footNote: `
+  //     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
+  //     Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
+  //   `,
+};
