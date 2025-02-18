b3d2b13e2de9f59940a2d36cb33ef55ac3c57b32
spongebobsmartypants <spongebobsmartypants2000@gmail.com>
2025-02-18 07:42:39 -0500

    Update Contact.tsx

--- src/solidComponents/widgets/Contact.tsx
+++ src/solidComponents/widgets/Contact.tsx
@@ -48,9 +48,25 @@ const Contact: Component = () => {
   };
 
   return (
-    <div class="flex flex-col max-w-xl mx-auto rounded-lg backdrop-blur border border-gray-200 dark:border-gray-700 bg-white dark:bg-slate-900 shadow p-4 sm:p-6 lg:p-8 w-full">
+    <div
+      class={
+        'flex flex-col max-w-xl mx-auto rounded-lg backdrop-blur border bg-white dark:bg-slate-900 shadow p-4 sm:p-6 lg:p-8 w-full ' +
+        (submitted() ? 'border-green-500' : 'border-gray-200 dark:border-gray-700')
+      }
+    >
       {submitted() ? (
-        <div class="py-8">Thank you {formData().name.split(' ')[0]}!</div>
+          <div class="flex items-center justify-center gap-1">
+            <p class="text-md font-semibold">Sent!</p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 text-green-500"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+            </svg>
+          </div>
       ) : (
         <form onSubmit={handleSubmit}>
           <Input label="Name" name="name" value={formData().name} onChange={handleChange} />
