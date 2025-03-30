ec07468648da78ba7fa0e103b85e713e2ce9a875
Hilary Torn <hilary.torn@gmail.com>
2025-03-30 10:51:50 +0300

    Updated footer to include Discord, YouTube, open in new tab

--- src/navigation.ts
+++ src/navigation.ts
@@ -197,8 +197,8 @@ export const footerData = {
   socialLinks: [
     // { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/pixlie' },
     { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://linkedin.com/company/pixlie' },
-    // { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    // { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/@PixlieTech' },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://discord.gg/5W9U9RPTGp' },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/pixlie' },
     // { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
   ],
